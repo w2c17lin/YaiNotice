62fa1e1d6ecd8ea730b074f9e40576df37cae506
yanji_ <w2c17lin@qq.com>
2017-08-24 15:29:07 +0800

    update

--- src/notice.js
+++ src/notice.js
@@ -2,20 +2,24 @@
 ! function () {
     "use strict";
 
-    const RIGHT = 24, // 初始位置
-        RIGHT_BEGIN = -360,
-        TOP_BEGIN = 256;
-
-    const INFO = 0, // 消息
-        SUCCESS = 1, // 成功
-        WARNING = 2, // 警告
-        ERROR = 3; // 错误
-
     const SPEED = 180; // 动画速度
+
+    const POSTION = {
+        RIGHT: 24,
+        RIGHT_BEGIN: -360,
+        TOP_BEGIN: 256
+    } // 初始位置
+
+    const TYPE = {
+        INFO: 0, // 消息
+        SUCCESS: 1, // 成功
+        WARNING: 2, // 警告
+        ERROR: 3 // 错误
+    }
 
     var options = {
         title: "消息",
-        type: INFO,
+        type: TYPE.INFO,
         timeout: 3000,
         content: undefined,
         callback: undefined
@@ -25,9 +29,9 @@
 
     function show(notice) {
         var type_class = "yai-notice-info";
-        if (notice.type == SUCCESS) type_class = "yai-notice-success";
-        else if (notice.type == WARNING) type_class = "yai-notice-warning";
-        else if (notice.type == ERROR) type_class = "yai-notice-danger";
+        if (notice.type == TYPE.SUCCESS) type_class = "yai-notice-success";
+        else if (notice.type == TYPE.WARNING) type_class = "yai-notice-warning";
+        else if (notice.type == TYPE.ERROR) type_class = "yai-notice-danger";
         var html =
             `<div class="yai-notice" data-id="${notice.id}">
                 <div class="yai-notice-container ${type_class}">
@@ -40,13 +44,13 @@
         var $notice = $(".yai-notice[data-id='" + notice.id + "']");
         var postion = yai_notice.length - 1;
         notice.height = $notice.height(); // 获取当前notice高度
-        notice.top = yai_notice[postion] ? yai_notice[postion].top + yai_notice[postion].height : TOP_BEGIN; // 获取当前notice的top
+        notice.top = yai_notice[postion] ? yai_notice[postion].top + yai_notice[postion].height : POSTION.TOP_BEGIN; // 获取当前notice的top
         yai_notice.push(notice);
         $notice.css({
             top: notice.top
         });
         $notice.animate({
-            right: RIGHT
+            right: POSTION.RIGHT
         }, SPEED);
         $notice.find(".yai-notice-close").on("click", () => remove(notice)); // 绑定移除按钮事件
         if (notice.timeout > 0) setTimeout(() => remove(notice), notice.timeout); // 设置取消定时
@@ -58,14 +62,14 @@
         if (notice.callback) notice.callback(); // 回调函数
         move(); // 滑动下面的提示
         $notice.animate({
-            right: RIGHT_BEGIN
+            right: POSTION.RIGHT_BEGIN
         }, SPEED, () => $notice.remove()); // 移除dom
     }
 
     function move() {
         for (var i = 0; i < yai_notice.length; i++) {
             var postion = i - 1;
-            yai_notice[i].top = yai_notice[postion] ? yai_notice[postion].top + yai_notice[postion].height : TOP_BEGIN;
+            yai_notice[i].top = yai_notice[postion] ? yai_notice[postion].top + yai_notice[postion].height : POSTION.TOP_BEGIN;
             $(".yai-notice[data-id='" + yai_notice[i].id + "']").animate({
                 top: yai_notice[i].top
             }, SPEED);
@@ -83,7 +87,7 @@
 
     YaiNotice.prototype.info = function (title, content) {
         this.open({
-            type: INFO,
+            type: TYPE.INFO,
             title: title,
             content: content
         });
@@ -91,7 +95,7 @@
 
     YaiNotice.prototype.success = function (title, content) {
         this.open({
-            type: SUCCESS,
+            type: TYPE.SUCCESS,
             title: title,
             content: content
         });
@@ -99,7 +103,7 @@
 
     YaiNotice.prototype.warning = function (title, content) {
         this.open({
-            type: WARNING,
+            type: TYPE.WARNING,
             title: title,
             content: content
         });
@@ -107,7 +111,7 @@
 
     YaiNotice.prototype.error = function (title, content) {
         this.open({
-            type: ERROR,
+            type: TYPE.ERROR,
             title: title,
             content: content
         });
